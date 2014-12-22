98cee21c968287b7851bab7620741871cb9d4110
Thomas Darde <thomas@rougecardinal.fr>
2014-12-22 14:33:26 +0100

    Load app_config with the gem

--- lib/rougecardinal_cms/helpers/seo_helper.rb
+++ lib/rougecardinal_cms/helpers/seo_helper.rb
@@ -1,12 +1,12 @@
 module SeoHelper
 
   def page_title
-    title = @page_title ? "#{@page_title} - XTG" : 'Xavier Trannoy Graphisme'
+    title = @page_title ? "#{@page_title} - #{APP_CONFIG[:seo]["title"]["suffix"]}" : APP_CONFIG[:seo]["title"]["default"]
     content_tag :title, title
   end
 
   def page_description
-    description = @page_description ? @page_description : 'XTG'
+    description = @page_description ? @page_description : APP_CONFIG[:seo]["description"]["default"]
     tag :meta, name: 'description', content: description
   end
 
--- lib/rougecardinal_cms/railtie.rb
+++ lib/rougecardinal_cms/railtie.rb
@@ -1,7 +1,11 @@
 require 'rougecardinal_cms/helpers/seo_helper'
 module RougecardinalCms
   class Railtie < Rails::Railtie
-    initializer "rougecardinal_cms.view_helpers" do
+    initializer "rougecardinal_cms.load_app_config" do
+      raw_config = File.read("config/app_config.yml")
+      ::APP_CONFIG = YAML.load(raw_config)[Rails.env].symbolize_keys
+    end
+    initializer "rougecardinal_cms.load_helpers" do
       ActionView::Base.send :include, SeoHelper
     end
   end
